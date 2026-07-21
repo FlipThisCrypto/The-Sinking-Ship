f47c18c950b65745579b105b4d2fa24e345cc7af
FlipThisCrypto <flipthiscrypto@gmail.com>
2026-07-21 16:43:58 -0400

    round 3 iteration 42: codify max concurrent reconcile hosts and backup cadence constants

---
+++ engine/fulfillment/ops_limits.py
@@ -0,0 +1,5 @@
+﻿# SPDX-License-Identifier: MIT
+"""Documented max concurrent reconcile hosts (ops constraint)."""
+MAX_RECONCILE_HOSTS = 1  # exclusive lock enforces this
+RECOMMENDED_BACKUP_INTERVAL_MIN = 15
+RECOMMENDED_METRICS_INTERVAL_MIN = 5
